1926c28e0a2e3fe21ea1f5fc9478b4f25538a15d
@emanuelrosa <fenix.gusmao+github@gmail.com>
2019-03-14 12:30:16 -0300

    update CURl with one command

--- bin/botapp.sh
+++ bin/botapp.sh
@@ -3,14 +3,7 @@
 
 echo "=> RUN | start app.sh"
 
-curl \
-  --request POST \
-  --url "${GLITCH_URL}/project/githubImport?authorization=${GLITCH_TOKEN}&projectId=${GLITCH_PROJECT_ID}&repo=${GLITCH_REPO}&path=${GLITCH_PATH}" \
-  --header "accept: */*" \
-  --header "origin: https://${APP_DOMAIN}" \
-  --header "referer: https://${APP_DOMAIN}/edit/" \
-  --header "cache-control: no-cache" \
-
+curl "https://api.${GLITCH_URL}/project/githubImport?authorization=${GLITCH_TOKEN}&projectId=${GLITCH_PROJECT_ID}&repo=${GLITCH_REPO}&path=${GLITCH_PATH}" -X POST -H "Accept: */*" -H "Referer: https://${GLITCH_URL}/edit/" -H "Origin: https://${GLITCH_URL}"
 
 echo "=> RUN | end app.sh"
 
